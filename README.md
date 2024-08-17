8d2983cd4a582859c396f661e29d06777966d004
Athan Massouras <amassouras@berkeley.edu>
2024-08-17 17:59:56 +0300

    make path loop (game loop 1) done

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # CLI-Spline
 
-A small CLI game using cubic spline interpolation (pronounced *klie-spline*). Make a path for a series of ants to follow!
+A small CLI game using cubic spline interpolation (pronounced *klie-spline*). Make a path out of pebbles for some ants to follow!
+
+## Build
+To install and build, clone the repository and use `build.sh`:
+```
+$ git clone https://github.com/Athan13/cli-spline.git
+$ bash build.sh
+```
+
+You can now run the game with `./cli-spline`.
+
+Dependencies are ncurses and GSL, `build.sh` is written for gcc.
+
+## How It Works
--- src/cli-spline.c
+++ src/cli-spline.c
@@ -1,56 +1,131 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 
 #include <gsl/gsl_matrix.h>
 #include <ncurses.h>
 
+#define INSTRUCT_HEIGHT 5
+#define MAX_PEBBLES 50
+#define MAX_ANTS 50
+
+#define ANT_CH 'O'
+#define PEBBLE_CH 'X'
+
 typedef struct Ant{
-    uint32_t x;
-    uint32_t y;
-    uint32_t t;
+    size_t x;
+    size_t y;
+    size_t t;
 } Ant;
 
-// Loop for creating the path that the ants follow.
-int make_path_loop(WINDOW *instruction_window, WINDOW *game_window) {
-    box(instruction_window, 0, 0);
+// Game loop for creating the path that the ants follow.
+// Returns the x-y coordinates of the pebbles.
+gsl_vector* make_path_loop(WINDOW *instruction_window, WINDOW *game_window) {
+    // Create and draw instruction window
+    mvwaddstr(instruction_window, 1, 1, "To move the pebble, use H (left), J (down), K (up), L (right)");
+    mvwaddstr(instruction_window, 2, 1, "Press SPACE to drop the pebble, and to get a new one.");
+    mvwaddstr(instruction_window, 3, 1, "Press ENTER when you're done dropping pebbles.");
 
+    box(instruction_window, 0, 0);
     wrefresh(instruction_window);
 
-    while(1) {
+    // Create main window
+    struct timespec remaining, rest = {0, 5E7};
+
+    gsl_vector *pebbles_xy = gsl_vector_alloc(MAX_PEBBLES * 2);
+    size_t num_pebbles = 0;
+
+    size_t cursor_x = 1, cursor_y = 1;
+    int input_ch;
+
+    while(1) {       
+        // Draw game window
+        werase(game_window);
+
+        for (size_t i = 0; i < num_pebbles; i++) {
+            size_t x = (size_t) pebbles_xy->data[2 * i];
+            size_t y = (size_t) pebbles_xy->data[2 * i + 1];
+            mvwaddch(game_window, y, x, PEBBLE_CH);
+        }
+
+        mvwaddch(game_window, cursor_y, cursor_x, PEBBLE_CH);
+        
         box(game_window, 0, 0);
+
         wrefresh(game_window);
-        break;
+        nanosleep(&rest, &remaining);
+
+        // Get keyboard input
+        input_ch = wgetch(game_window);
+        switch(input_ch) {
+            case 'h':
+                mvwaddstr(game_window, 1, 1, "LEFT");
+                cursor_x -= (cursor_x == 1) ? 0 : 1;
+                break;
+            case 'j':
+                mvwaddstr(game_window, 1, 1, "DOWN");
+                cursor_y += (cursor_y == LINES - INSTRUCT_HEIGHT - 2) ? 0 : 1;
+                break;
+            case 'k':
+                mvwaddstr(game_window, 1, 1, "UP");
+                cursor_y -= (cursor_y == 1) ? 0 : 1;
+                break;
+            case 'l':
+                mvwaddstr(game_window, 1, 1, "RIGHT");
+                cursor_x += (cursor_x == COLS - 2) ? 0 : 1;
+                break;
+            case ' ':
+                if (num_pebbles == MAX_PEBBLES) break;
+                pebbles_xy->data[2 * num_pebbles] = cursor_x;
+                pebbles_xy->data[2 * num_pebbles + 1] = cursor_y;
+                num_pebbles++;
+                break;
+            case '\n':
+                if (num_pebbles < 2) break;
+                return pebbles_xy;
+            default:
+                break;
+        } 
     }
 
-    return 0;
+    return NULL;
 }
 
-// Loop for the main part of the game: seeing the ants follow the path.
+// Game loop for the main part of the game: seeing the ants follow the path.
 int ants_loop() {
     return 0;
 }
 
 int main(int argc, char** argv) {
 
-    // initialise ncurses
+    // Initialise ncurses
     initscr();
-
     noecho();
     cbreak();
+    curs_set(0);
 
-    WINDOW *instructions_window = newwin(5, COLS, 0, 0);
-    WINDOW *game_window = newwin(LINES - 5, COLS, 5, 0);
+    WINDOW *instruction_window = newwin(INSTRUCT_HEIGHT, COLS, 0, 0);
+    WINDOW *game_window = newwin(LINES - INSTRUCT_HEIGHT, COLS, INSTRUCT_HEIGHT, 0);
 
-    nodelay(instructions_window, true);
+    nodelay(instruction_window, true);
     nodelay(game_window, true);
 
-    make_path_loop(instructions_window, game_window);
-    sleep(2);
+    // Game loop 1
+    gsl_vector *pebbles_xy = make_path_loop(instruction_window, game_window);
 
-    // Cleanup ncurses
+    // Linear algebra for splines
+
+    // Game loop 2 (main game loop)
+
+    // Cleanup - ncurses
     endwin();
-    delwin(instructions_window);
+    delwin(instruction_window);
     delwin(game_window);
+
+    curs_set(2);
+
+    // Cleanup - vectors
+    gsl_vector_free(pebbles_xy);
 
     return 0;
 }
