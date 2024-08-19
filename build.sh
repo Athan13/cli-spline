02812fab14841d0444659af3fdc6d42b678e902e
Athan Massouras <amassouras@berkeley.edu>
2024-08-19 20:27:26 +0300

    build and test fix

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-gcc -Wall -c -I/opt/homebrew/include src/spline-calc.c -o spline-calc.o
-gcc -Wall -c -I/opt/homebrew/include src/cli-spline.c -o cli-spline.o
+gcc -Wall -c src/spline-calc.c -o spline-calc.o
+gcc -Wall -c src/cli-spline.c -o cli-spline.o
 gcc -Wall spline-calc.o cli-spline.o -lgsl -gslcblas -lncurses -o cli-spline.out
 
 rm -f spline-calc.o cli-spline.o
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-gcc -Wall -g -c -I/opt/homebrew/include src/spline-calc.c -o spline-calc.o
-gcc -Wall -g -c -I/opt/homebrew/include test/spline-calc-test.c -o spline-test.o
+gcc -Wall -g -c src/spline-calc.c -o spline-calc.o
+gcc -Wall -g -c test/spline-calc-test.c -o spline-test.o
 gcc -Wall -g spline-calc.o spline-test.o -lgsl -gslcblas -o cli-spline-test.out
 
 rm -f spline-calc.o spline-test.o
