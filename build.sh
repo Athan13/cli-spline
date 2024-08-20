ef6392a9c4636d3a42a9ce5a1e002c5f7ce9e21e
Athan Massouras <amassouras@berkeley.edu>
2024-08-20 10:48:46 +0300

    fix build and test for linux

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 gcc -Wall -c src/spline-calc.c -o spline-calc.o
 gcc -Wall -c src/cli-spline.c -o cli-spline.o
-gcc -Wall spline-calc.o cli-spline.o -lgsl -gslcblas -lncurses -o cli-spline.out
+gcc -Wall spline-calc.o cli-spline.o -lgsl -lgslcblas -lncurses -o cli-spline.out
 
 rm -f spline-calc.o cli-spline.o
--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 gcc -Wall -g -c src/spline-calc.c -o spline-calc.o
 gcc -Wall -g -c test/spline-calc-test.c -o spline-test.o
-gcc -Wall -g spline-calc.o spline-test.o -lgsl -gslcblas -o cli-spline-test.out
+gcc -Wall -g spline-calc.o spline-test.o -lgsl -lgslcblas -o cli-spline-test.out
 
 rm -f spline-calc.o spline-test.o
 
